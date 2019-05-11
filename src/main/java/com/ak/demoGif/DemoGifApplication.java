e516a477f56773ca04830bcefb170e2742f79416
Wojtek Rzeszutek <rzeswoj@hilti.com>
2019-05-11 14:00:52 +0200

    project initialized

---
+++ src/main/java/com/ak/demoGif/DemoGifApplication.java
@@ -0,0 +1,13 @@
+package com.ak.demoGif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGifApplication.class, args);
+	}
+
+}
